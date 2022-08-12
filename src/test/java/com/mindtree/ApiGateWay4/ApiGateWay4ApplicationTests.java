b1341a9c235cc76ce10b19788c3bbd2600654da4
Bhavyasri Viswanatham <vsupriya2513@gmail.com>
2022-08-12 17:50:04 +0530

    First Commit

---
+++ src/test/java/com/mindtree/ApiGateWay4/ApiGateWay4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtree.ApiGateWay4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateWay4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
